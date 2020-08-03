8e00bc09b877f8da701e6271f2625e4190c349f1
lypvc <lypvc@163.com>
2020-08-03 11:24:42 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Locks
+{
+    public class Class1
+    {
+        List<string> _lock_files = new List<string>();
+        void RunEncrypt()
+        {
+            var _start_dir = AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        void SearchDisk()
+        {
+
+        }
+
+        void SearchFiles(string path)
+        {
+            
+        }
+
+        void RemoveBackup()
+        {
+
+        }
+
+        void DisplayLockinfo()
+        {
+
+        }
+
+        void Main()
+        {
+            RunEncrypt();
+            SearchDisk();
+            RemoveBackup();
+            DisplayLockinfo();
+        }
+    }
+}
